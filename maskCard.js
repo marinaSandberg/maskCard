536b46caaaa0fd0c10c95d38468f748f09c86c13
marinaSandberg <marina_p13@hotmail.com>
2023-04-02 19:27:59 +0200

    html and css page

--- maskCard.js
+++ maskCard.js
@@ -1,38 +1,40 @@
-let cardNumber = "AB12 3456 7890 1234";
+function maskCard() {
+    let cardNumber = document.querySelector("input").value;
+    let exportCard = document.querySelector("#returnCard");
 
+    if (cardNumber.length < 6 ) {
+        exportCard.textContent = cardNumber;
+        return cardNumber = " ";
+    }
 
-if (cardNumber < "6") {
-    let maskCard = cardNumber;
+    //----------1st way----------
 
-    console.log(maskCard);
-}
+    else if (cardNumber.length >= 6 ) {
+        // Select first character
+        let first = cardNumber.substring(0,1);
 
-//----------1st way----------
+        // Select rest card charaters and replace all numbers but the last 4
+        let maskedNumbers = cardNumber.substring(1).replace(/[0-9](?=.{4})/g,"*");
 
-if (cardNumber >= "6") {
-    // Select first character
-    let first = cardNumber.substring(0,1);
+        // Concatenate the masked string
+        let maskCard = first.concat(maskedNumbers);
 
-    // Select rest card charaters and replace all numbers but the last 4
-    let maskedNumbers = cardNumber.substring(1).replace(/[0-9](?=.{4})/g,"*");
+        exportCard.textContent = maskCard;
+        return cardNumber = " ";
+    }
 
-    // Concatenate the masked string
-    let maskCard = first.concat(maskedNumbers);
+    //----------2nd way----------
 
-    console.log(maskCard); // Output: "AB** **** **** 1234"
-}
+    // if (cardNumber >= "6") {
+    //     // Exclude 1st and last 4 digits
+    //     let maskedNumber = cardNumber.slice(1, -4);
 
-//----------2nd way----------
+    //     // Mask all numbers from the string
+    //     let numbersOnly = maskedNumber.replace(/[0-9]/g, "*");
 
-if (cardNumber >= "6") {
-    // Exclude 1st and last 4 digits
-    let maskedNumber = cardNumber.slice(1, -4);
+    //     //Concatenate 1st and last 4 digits with masked numbers
+    //     let maskedCard = cardNumber.slice(0,1).concat(numbersOnly).concat(cardNumber.slice(-4))
 
-    // Mask all numbers from the string
-    let numbersOnly = maskedNumber.replace(/[0-9]/g, "*");
-
-    //Concatenate 1st and last 4 digits with masked numbers
-    let maskedCard = cardNumber.slice(0,1).concat(numbersOnly).concat(cardNumber.slice(-4))
-
-    console.log(maskedCard); // Output: "AB** **** **** 1234"
+    //     console.log(maskedCard); // Output: "AB** **** **** 1234"
+    // }
 }
