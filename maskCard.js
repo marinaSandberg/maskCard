27c72f26ea11a690a18c0dd8f771a331f8bc573d
marinaSandberg <marina_p13@hotmail.com>
2023-04-06 13:43:43 +0200

    updated layout

--- maskCard.js
+++ maskCard.js
@@ -1,40 +1,46 @@
 function maskCard() {
-    let cardNumber = document.querySelector("input").value;
-    let exportCard = document.querySelector("#returnCard");
 
-    if (cardNumber.length < 6 ) {
-        exportCard.textContent = cardNumber;
-        return cardNumber = " ";
-    }
+    // Pick up card number
+   let cardNumberInput = document.querySelector("#input");
+       let cardNumber = cardNumberInput.value;
 
-    //----------1st way----------
+       if (cardNumber.length > 6 ) {
+           // Select first character
+           let first = cardNumber.substring(0,1);
 
-    else if (cardNumber.length >= 6 ) {
-        // Select first character
-        let first = cardNumber.substring(0,1);
+           // Select rest card charaters and replace all numbers but the last 4
+           let maskedNumbers = cardNumber.substring(1).replace(/[0-9](?=.{4})/g,"*");
 
-        // Select rest card charaters and replace all numbers but the last 4
-        let maskedNumbers = cardNumber.substring(1).replace(/[0-9](?=.{4})/g,"*");
+           // Concatenate the masked string
+           let maskedCard = first.concat(maskedNumbers);
 
-        // Concatenate the masked string
-        let maskCard = first.concat(maskedNumbers);
+           // Export masked card
+           cardNumberInput.value = maskedCard;
+           console.log(maskedCard);
+       }
+       
+       // If the number is short
+       else {
+           cardNumberInput.value = cardNumber;
+       }
 
-        exportCard.textContent = maskCard;
-        return cardNumber = " ";
-    }
+   // ----------2nd way----------
 
-    //----------2nd way----------
+   // if (cardNumber.length > 6) {
+   //     // Exclude 1st and last 4 digits
+   //     let maskedNumber = cardNumber.slice(1, -4);
 
-    // if (cardNumber >= "6") {
-    //     // Exclude 1st and last 4 digits
-    //     let maskedNumber = cardNumber.slice(1, -4);
+   //     // Mask all numbers from the string
+   //     let numbersOnly = maskedNumber.replace(/[0-9]/g, "*");
 
-    //     // Mask all numbers from the string
-    //     let numbersOnly = maskedNumber.replace(/[0-9]/g, "*");
+   //     //Concatenate 1st and last 4 digits with masked numbers
+   //     let maskedCard = cardNumber.slice(0,1).concat(numbersOnly).concat(cardNumber.slice(-4))
 
-    //     //Concatenate 1st and last 4 digits with masked numbers
-    //     let maskedCard = cardNumber.slice(0,1).concat(numbersOnly).concat(cardNumber.slice(-4))
+   //     exportCard.textContent = maskedCard;
+   // }
+}
 
-    //     console.log(maskedCard); // Output: "AB** **** **** 1234"
-    // }
+function Reset () {
+    let form = document.querySelector("form");
+    form.reset();
 }
